6527af8a8c5ba0e8f303ed72138883ad06279016
thuyluu <thuyluu9595@gmail.com>
2021-12-28 14:40:52 -0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>{
+    console.log('Serve http://localhost:${port}');
+});
